52ada010fb00022bf4238cd8902737c71d5ed75c
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-17 00:24:18 -0300

    Criada lógica de iteração para cálculo de raiz quadrada

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-raiz = 5
+import numpy as np
+
+a = 5
+x0 = 2
+err = 0.001
+
+x1 = 0.5*(x0 + a/x0)
+
+tol = np.absolute( (x1 - x0)/x1 )
+
+
+while tol > err:
+    x1 = 0.5*(x0 + a/x0)
+    tol = np.absolute( (x1 - x0)/x1 )
+    x0 = x1
+    print("Tol = ",tol)
+    print("xn: ",x1)
+    print()
+
+
+print(f"O valor da raíz é {x1:.6f}: ")
